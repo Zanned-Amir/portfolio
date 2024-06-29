84f5e572c7ee4e3506347d7eacf8ccdb5ae63e8b
jingaba <amirosolaroso34@gmail.com>
2024-06-29 01:45:00 +0100

    test

--- script.js
+++ script.js
@@ -5,20 +5,6 @@ checkbox.addEventListener("change", () => {
 
 
 
-let sections = document.querySelector('section');
-let  navlinks = document.querySelectorAll('nav a');
-
-window.addEventListener(sec => {
-  let top = Window.scrollY ;
-  let offset =  sec.offestTop ;
-  let height = sec.offsetHeight;
-  let id = sec.getAttribute('id');
-
-  if(top >= offset && top < offset + height){
-    navlinks.forEach(links => {
-      links.classList.remove('active');
-      document.querySelector('nav a[href*=' + id + ']').classList.add('active');
-    });
-  }
+window.onload = function() {
+document.body.classList.add("dark");
 }
-);
